5c411b8c94acff3cb59192be92200f72b5ed8d9d
Philip McGrath <philip@philipmcgrath.com>
2020-04-12 19:52:29 -0400

    attempt at i386-macosx --- not yet tested

--- portaudio/lib/cc.rkt
+++ portaudio/lib/cc.rkt
@@ -11,17 +11,37 @@
 (define-runtime-path callbacks.c
   "callbacks.c")
 
-(define so-path
-  (let-values ([{base _name _dir} (split-path callbacks.c)])
-    (build-path base
-                (system-library-subpath #f)
-                (append-extension-suffix "callbacks"))))
+(define lib/
+  (path-only callbacks.c))
 
-(define (cc)
-  (parameterize ([current-use-mzdyn #f])
-    (when (file-exists? so-path)
+(define so-path-elem
+  (append-extension-suffix "callbacks"))
+
+(define (cc [so-path (build-path lib/
+                                 (system-library-subpath #f)
+                                 so-path-elem)]
+            #:overwrite? [overwrite? #t])
+  (parameterize ([current-use-mzdyn #f]
+                 [current-extension-linker-flags
+                  (if (eq? 'macosx (system-type 'os))
+                      (list* "-mmacosx-version-min=10.5"
+                             (current-extension-linker-flags))
+                      (current-extension-linker-flags))])
+    (when (and overwrite? (file-exists? so-path))
       (delete-file so-path))
     (make-parent-directory* so-path)
     (link-extension #f ;; not quiet
                     (list callbacks.c)
                     so-path)))
+
+(define (cc/i386-macosx)
+  (unless (eq? 'macosx (system-type 'os))
+    (raise-arguments-error 'cc/i386-macosx
+                           "unsupported system type"
+                           "expected" 'macosx
+                           "given" (system-type 'os)))
+  (parameterize ([current-extension-linker-flags
+                  (list* "-arch"
+                         "i386"
+                         (current-extension-linker-flags))])
+    (cc (build-path lib/ "i386-macosx" so-path-elem))))
